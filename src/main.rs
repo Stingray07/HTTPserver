5b232638a414abd7c4b6e901d6bcaa5b9c51bfd0
Stingray <stingraysonggay@gmail.com>
2025-05-21 09:12:31 +0800

    Added routing and parsing | TODO body for POST

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::net::TcpListener;
 use std::io::{Read, Write};
 
+mod routes;
+
+struct HttpRequest{
+    method: String,
+    path: String, 
+    version: String,
+    body: String,
+    headers: Vec<(String, String)>,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -11,12 +20,21 @@ fn main() {
 
         // this is where the request was put
         let mut buffer = [0; 512];
+
+        // read the request into the buffer
         stream.read(&mut buffer).unwrap();
 
         println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        println!("================================");
 
-        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\
-                                      <html><body><h1>Hello, world!</h1></body></html>";
+        let parsed_request = parse_request(&mut buffer);
+
+        // matching for routing
+        let response: String = match parsed_request.path.as_str() {
+            "/" => routes::handle_home(),
+            "/about" => routes::handle_about(),
+            _ => routes::handle_404(),
+        };
 
         stream.write(response.as_bytes()).unwrap();
 
@@ -24,3 +42,25 @@ fn main() {
         stream.flush().unwrap();
     }
 }
+
+fn parse_request(buffer: & [u8]) -> HttpRequest {
+    let request_string = String::from_utf8_lossy(&buffer[..]);
+    let mut lines = request_string.lines();
+
+    let request_line = lines.next().unwrap();
+    let mut parts = request_line.split_whitespace();
+    
+    let req = HttpRequest {
+    method: parts.next().unwrap().to_string(),
+    path: parts.next().unwrap().to_string(),
+    version: parts.next().unwrap().to_string(),
+    body: "".to_string(),
+    headers: lines
+        .filter_map(|line| line.split_once(':'))
+        .map(|(k, v)| (k.trim().to_string(), v.trim().to_string()))
+        .collect(),
+    };
+
+    return req
+}
+
---
+++ src/routes.rs
@@ -0,0 +1,14 @@
+pub fn handle_about() -> String {
+    "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\
+    <html><body><h1>About</h1></body></html>".to_string()
+}
+
+pub fn handle_home() -> String {
+    "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\
+    <html><body><h1>HOME</h1></body></html>".to_string()
+}
+
+pub fn handle_404() -> String {
+    "HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n\
+        <html><body><h1>404 Page Not Found</h1></body></html>".to_string()
+}
